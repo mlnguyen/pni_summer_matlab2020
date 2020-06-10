94d5603ebf1ff82bb126d08bbaac5c915d102bec
Mai Nguyen <mlin.nguyen@gmail.com>
2020-06-10 15:51:28 -0400

    add lecture 1 materials

---
+++ week1/week1_lecture1_script.m
@@ -0,0 +1,54 @@
+
+%% Running scripts
+% 1. Run button
+% 2. Run a section if you have %% sections
+% 3. Run line by line by highlighting
+
+
+%% CALCULATES THE AREA OF A RECTANGLE WITH A GIVEN LENGTH AND WITH
+
+% Created variables length and width, and assigned their values
+length = 10;
+width = 3;
+
+% Calculate the area of a rectangle: area = length * width
+area = length * width;
+
+% Print answer to the command window
+disp(area)
+
+%% Calculate the area of a circle with radius 3
+% built-in constant: pi
+% declare your varriables, write your eq, display the answer
+
+% assign value to radius
+radius = 3;
+
+% calculate circumference
+circumference = 2*pi*radius;
+
+% Calculate area
+area = pi * radius^2;
+
+% display results
+disp(area)
+disp(circumference)
+
+%% another new section
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
